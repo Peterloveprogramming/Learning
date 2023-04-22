3a4845e22dccd5c195360063519df4e2d3fd035e
Peterloveprogramming <131538732+Peterloveprogramming@users.noreply.github.com>
2023-04-23 00:47:27 +1000

    Add files via upload

---
+++ C/Adding 2 Numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number1;
+    int number2;
+
+    printf("Enter first number\n");
+    scanf("%d",&number1);
+    printf("Enter second number\n");
+    scanf("%d",&number2);
+    printf("The sum is %d",number1+number2);
+
+    return 0;
+
+}
