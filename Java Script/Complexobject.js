f57cb180141ef85bba46d6f394d53ef83d072d8f
Peterloveprogramming <131538732+Peterloveprogramming@users.noreply.github.com>
2023-04-24 00:02:45 +1000

    Create Complexobject.js

---
+++ Java Script/Complexobject.js
@@ -0,0 +1,15 @@
+var mystorage = {
+    "Car": {
+        "Inside":{
+            "gloveBox": "Map",
+            "PaasengerSeat": "Torch"
+            
+        },
+        "outside":{
+            "Trunk":"Jacket"
+        }
+        
+    }
+};
+
+console.log(mystorage.Car.Inside.gloveBox)
