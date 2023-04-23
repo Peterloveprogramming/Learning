035b936c9ff90800d88a278817e50abece4b61cf
Peterloveprogramming <131538732+Peterloveprogramming@users.noreply.github.com>
2023-04-23 18:29:45 +1000

    Create ifstatement.js

---
+++ Java Script/ifstatement.js
@@ -0,0 +1,11 @@
+function testNumberFunction (number){
+    if (number <5){
+        return "Less than 5"
+    } else if(number<10){
+        return "less than 10"
+    } else{
+        return"Greater or euqal to 10"
+    }
+}
+
+console.log(testNumberFunction(11))
