b634fea78176db7aa33e54a518502eda5c31b9f2
Peterloveprogramming <131538732+Peterloveprogramming@users.noreply.github.com>
2023-04-25 17:27:30 +1000

    Create FilteredArray.js

---
+++ Java Script/FilteredArray.js
@@ -0,0 +1,13 @@
+const PLAYERS =[
+    {"name":"Peter", 
+    "hobby":"Javascript",
+    "Age":24
+    },
+    {"name":"Jack",
+    "hobby":"Finance",
+    Age:30
+    }
+];
+
+const filteredArray = PLAYERS.filter(player => player.Age>25);
+console.log(filteredArray)
