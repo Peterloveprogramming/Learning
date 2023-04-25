c529b6ac2c4b906c001a48a10fad9f68d49f3d8c
Peterloveprogramming <131538732+Peterloveprogramming@users.noreply.github.com>
2023-04-25 17:50:26 +1000

    Update FilteredArray.js

--- Java Script/FilteredArray.js
+++ Java Script/FilteredArray.js
@@ -11,3 +11,15 @@ const PLAYERS =[
 
 const filteredArray = PLAYERS.filter(player => player.Age>25);
 console.log(filteredArray)
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const Numbers = [-1,0.2,2,3,4,-2,-12,2.3,4.2];
+// I want to remove all the negative and decimals 
+// then sqaure the numbers 
+
+const filteredarray = Numbers.filter(n=> Number.isInteger(n)&& n>0);
+const updatedarray = filteredarray.map(n => n*n);
+
+console.log(updatedarray)
