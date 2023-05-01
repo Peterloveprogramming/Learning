d1395ae1adba1487a2868d8f24892a4376e34b98
Peterloveprogramming <131538732+Peterloveprogramming@users.noreply.github.com>
2023-05-01 21:34:14 +1000

    Create Find First X.js

---
+++ Coding Challenge/Alchemy/Find First X.js
@@ -0,0 +1,11 @@
+Your Goal: Find the First X
+In the string argument find the index of the first lower-case "x" and return it.
+--
+
+function findFirstX(string) {
+    let b = string.indexOf("x")
+    return b
+}
+
+module.exports = findFirstX;
+
