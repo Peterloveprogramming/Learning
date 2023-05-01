ea404e2aeafef65c918afea8d23c8951016cfedc
Peterloveprogramming <131538732+Peterloveprogramming@users.noreply.github.com>
2023-05-01 20:40:36 +1000

    Create is numbereven?.js

---
+++ Coding Challenge/Alchemy/is numbereven?.js
@@ -0,0 +1,19 @@
+Instructions:
+
+Your Goal: Is It Even?
+Given an integer value num, determine if it is even.
+If it is even, return true. Return false otherwise.
+Example Usage:
+
+console.log( isEven(1) ); // false
+console.log( isEven(4) ); // true
+
+---
+  
+  
+function isnumbereven (x){
+    if(x%2 === 0){
+        return true
+    }
+    return false
+}
