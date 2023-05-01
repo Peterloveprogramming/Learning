2f82cd270b7908bc8d0741945c049a19f0109d59
Peterloveprogramming <131538732+Peterloveprogramming@users.noreply.github.com>
2023-05-01 20:44:12 +1000

    Create whichNumberIsSmaller.js

---
+++ Coding Challenge/Alchemy/whichNumberIsSmaller.js
@@ -0,0 +1,19 @@
+Your Goal: Return Smaller
+The function smallerNumber will be given two unequal numbers: num1 and num2.
+Your goal is to find the smaller number and return it!
+Example Usage:
+
+console.log( smallerNumber(3, 5) ); // 3
+
+----
+
+
+function smallerNumber(num1, num2) {
+    if (num1<num2){
+        return num1
+    }else {
+        return num2
+    }
+}
+
+module.exports = smallerNumber;
