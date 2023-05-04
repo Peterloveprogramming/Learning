7a09f1e7bb8f437c711d3ba49470d4a1c91e5a8a
Peterloveprogramming <131538732+Peterloveprogramming@users.noreply.github.com>
2023-05-04 17:45:03 +1000

    Create Boo who.js

---
+++ Coding Challenge/freeCodeCamp/Basic Algorithm Scripting/Boo who.js
@@ -0,0 +1,14 @@
+Check if a value is classified as a boolean primitive. Return true or false.
+
+Boolean primitives are true and false.
+
+---
+  
+  function booWho(bool) {
+  let c = 3>2;
+  if (typeof(bool)=== typeof(c)){
+      return true
+  }
+  return false
+}
+booWho(null);
