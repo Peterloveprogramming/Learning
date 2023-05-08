b3d7d573c938381d58708b3dcdfdffe8faf51771
Peterloveprogramming <131538732+Peterloveprogramming@users.noreply.github.com>
2023-05-08 18:28:25 +1000

    Create Return Part of an Array Using the slice Method.js

---
+++ Coding Challenge/freeCodeCamp/functional programming/Return Part of an Array Using the slice Method.js
@@ -0,0 +1,6 @@
+Use the slice method in the sliceArray function to return part of the anim array given the provided beginSlice and endSlice indices.
+The function should return an array.
+
+---
+  
+  Use the slice method in the sliceArray function to return part of the anim array given the provided beginSlice and endSlice indices. The function should return an array.
